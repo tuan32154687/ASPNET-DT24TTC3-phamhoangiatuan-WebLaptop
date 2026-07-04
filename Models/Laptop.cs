0bbc2e439fb020e6103e615b5b1c2ff51a8e2a44
tuan32154687 <tuan32154687@gmail.com>
2026-07-04 23:57:48 +0700

    feat: add Laptop model

---
+++ Models/Laptop.cs
@@ -0,0 +1,10 @@
+﻿namespace LaptopStore.Models
+{
+    public class Laptop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+    }
+}
