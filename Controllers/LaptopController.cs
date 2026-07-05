716929c1a1d3f066c7b5f6c93b24169277093878
tuan32154687 <tuan32154687@gmail.com>
2026-07-05 11:37:05 +0700

    Create Laptop Controller and View to display product list

---
+++ Controllers/LaptopController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LaptopStore.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaptopStore.Controllers
+{
+    public class LaptopController : Controller
+    {
+        private readonly LaptopDbContext _context;
+
+        public LaptopController(LaptopDbContext context)
+        {
+            _context = context;
+        }
+
+        // Action hiển thị danh sách
+        public async Task<IActionResult> Index()
+        {
+            var laptops = await _context.Laptops.ToListAsync();
+            return View(laptops);
+        }
+    }
+}
